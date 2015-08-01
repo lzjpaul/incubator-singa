a71173b747850f98bb75728797d757aab522322f
zhaojing <zhaojing@comp.nus.edu.sg>
2015-08-01 20:14:15 +0800

    python code

--- examples/SynPUF/python/standardization.py
+++ examples/SynPUF/python/standardization.py
@@ -11,19 +11,17 @@ def readData(filename):
     return [line.rstrip().split(',') for line in open(filename)]
 
 #read training data
-train_data = readData("standardization_test.csv")  #modify here
+train_data = readData("standardization_example.csv")  #modify here
 train_matrix = np.array(train_data[0:])[:,0:]
 
 #define model
 print train_matrix.shape
-train_X, train_y = train_matrix[:,0:14].astype(np.float), train_matrix[:,15].astype(np.int) #modify here0:1017
+train_X, train_y = train_matrix[:,0:3].astype(np.float), train_matrix[:,3].astype(np.int) #modify here0:1018!!!!!!! less than 1018
+print train_X.shape
 #standardization
 scaler = preprocessing.StandardScaler().fit(train_X)
 train_X = scaler.transform(train_X)
 a = numpy.asarray(train_X, dtype = float)
 b= numpy.asarray(train_y, dtype = int)
-numpy.savetxt("standardization_testoutputfea.csv", a, fmt = '%6f', delimiter=",") #modify here
-numpy.savetxt("standardization_testoutputlabel.csv", b, fmt = '%d', delimiter=",") #modify here
-
-
-
+numpy.savetxt("standardization_examplefea.csv", a, fmt = '%6f', delimiter=",") #modify here
+numpy.savetxt("standardization_examplelabel.csv", b, fmt = '%d', delimiter=",") #modify here
