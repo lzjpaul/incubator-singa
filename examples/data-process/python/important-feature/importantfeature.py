6e2d9160d7c1df1d4c69c5fad9563e09acd4bf28
zhaojing <zhaojing@comp.nus.edu.sg>
2015-09-05 22:32:09 +0800

    add importantfeature.py

--- examples/data-process/python/important-feature/importantfeature.py
+++ examples/data-process/python/important-feature/importantfeature.py
@@ -29,20 +29,25 @@ for i in range(len(index)):
 
 scaler = preprocessing.StandardScaler().fit(selected_sample)
 selected_sample_std = scaler.std_
-print selected_sample_std
+#print selected_sample_std
+for i in range(len(selected_sample_std)):
+    print i, selected_sample_std[i]
 
 selected_feature_num = int (sys.argv[4])
 selected_feature_index = np.zeros(selected_feature_num)
-print selected_feature_index. shape
+print selected_feature_index.shape
+
+print sorted(selected_sample_std)
 
 j = 0
 for k in range(len(selected_sample_std)):
-    feature_std_value = sorted(selected_sample_std, reverse = True)[k]
+    feature_std_value = sorted(selected_sample_std)[k]
     for i in range(len(selected_sample_std)):
 	if selected_sample_std[i] == feature_std_value and j < selected_feature_num:
 	    selected_feature_index[j] = i
 	    j = j + 1
 
+print selected_feature_index
 # Write fitting result into output file
 file = open(sys.argv[3], "w")
 np.savetxt(file, selected_feature_index, "%d", ",")
