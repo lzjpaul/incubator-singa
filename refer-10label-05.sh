cac3e07f85b9295292aa51a7bc0007e9459ffdea
zhaojing <zhaojing@comp.nus.edu.sg>
2015-09-24 14:37:56 +0800

    before run marble

--- refer-10label-05.sh
+++ refer-10label-05.sh
@@ -1,3 +1,3 @@
-./bin/singa-run.sh -model=examples/SynPUF-regulariz/refer10-label/float-mlp2level-000001-decay-00001.conf -cluster=examples/SynPUF-regulariz/cluster.conf
+#./bin/singa-run.sh -model=examples/SynPUF-regulariz/refer10-label/float-mlp2level-000001-decay-00001.conf -cluster=examples/SynPUF-regulariz/cluster.conf
 ./bin/singa-run.sh -model=examples/SynPUF-regulariz/refer10-label/float-mlp2level-000001-decay-0.conf -cluster=examples/SynPUF-regulariz/cluster.conf
 ./bin/singa-run.sh -model=examples/SynPUF-regulariz/refer10-label/float-mlp2level-01-decay-001.conf -cluster=examples/SynPUF-regulariz/cluster.conf
