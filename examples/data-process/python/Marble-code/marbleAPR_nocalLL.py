0345d171ee7d254bb36a14a07951be721bfa420d
zhaojing <zhaojing@comp.nus.edu.sg>
2015-12-01 13:32:31 +0800

    debug marble

---
+++ examples/data-process/python/Marble-code/marbleAPR_nocalLL.py
@@ -0,0 +1,243 @@
+'''
+Compute the nonnegative tensor factorization using
+alternating Poisson regression
+
+This is the algorithm described in the paper Marble.
+'''
+import numpy as np
+import time
+from collections import OrderedDict
+
+from marble import *       # NOQA
+import tensorTools
+
+_DEF_MAXINNER = 10
+_DEF_GRADUAL = True
+_DEF_DELTATOL = 1e-2
+
+
+def project_A(A, gamma):
+    """
+    Project the signal factor A onto the feasible space
+    Basically hard-thresholding on the gamma
+    """
+    # first make sure they are above zero
+    A = np.maximum(A, np.zeros(A.shape))
+    # if they're all the same just return uniform
+    if len(np.unique(A)) == 1:
+        A = np.ones(A.shape)
+        A, _ = tensorTools.l1_normalize(A)
+        return A
+    # normalize
+    A, _ = tensorTools.l1_normalize(A)
+    # hard threshold
+    A = tensorTools.hardThresholdMatrix(A, gamma)
+    # do a quick sanity check
+    badCol = np.where(np.sum(A, axis=0) == 0)
+    A[:, badCol] = 1
+    A, _ = tensorTools.l1_normalize(A)
+    return A
+
+
+class MarbleAPR(Marble):
+    alpha = 0
+
+    def __init__(self, X, R, alpha):
+        self.obs_tensor = X
+        self.dim_num = X.ndims()
+        self.cp_rank = R
+        self.alpha = alpha
+
+    def normalizeAugTensor(self):
+        """
+        Normalize the augmented tensor to the value alpha
+        """
+        self.cp_decomp[AUG_LOCATION].normalize(1)
+        self.cp_decomp[AUG_LOCATION].lmbda = np.repeat(self.alpha, 1)
+
+    def _solveMode(self, Pi, B, C, n, maxInnerIters, convTol=1e-4):
+        """
+        Performs the inner iterations and checks for convergence.
+        """
+        for innerI in range(maxInnerIters):
+            # Phi = (X(n) elem-div (B Pi)) Pi^T
+            # print "B shape = \n", B.shape
+            # print "Pi shape = \n", Pi.shape
+            Phi = tensorTools.calculatePhi(self.obs_tensor, B, Pi, n, C=C)
+            # check for convergence that min(B(n), E - Phi(n)) = 0 [or close]
+            # kktModeViolation = np.max(np.abs(np.minimum(B, 1 - Phi).flatten()))
+            # if (kktModeViolation < convTol):
+            #    break
+            # Do the multiplicative update
+            print "B shape = \n", B.shape
+            print "Phi shape = \n", Phi.shape
+            B = np.multiply(B, Phi)
+        return B, (innerI + 1), 1e-4
+
+    def solveSubproblem(self, C, aug, n, maxInnerIters):
+        """
+        Solve the subproblem for mode n
+
+        Parameters
+        ------------
+        C : the "other" tensor, either bias or signal tensor
+        aug : the location to solve (either augmented or not)
+        n : the mode to solve
+        """
+        # shift the weight from lambda to mode n
+        self.cp_decomp[aug].redistribute(n)
+        Pi = tensorTools.calculatePi(self.obs_tensor, self.cp_decomp[aug], n)
+        # print "solve Subproblem n = \n", n
+        # print "cp_decomp[aug].U[n] shape = \n", self.cp_decomp[aug].U[n].shape
+        B, inI, kktModeViolation = self._solveMode(Pi,
+                                                   self.cp_decomp[aug].U[n],
+                                                   C, n, maxInnerIters)
+        self.cp_decomp[aug].U[n] = B
+        # shift the weight from mode to lambda
+        self.cp_decomp[aug].normalize_mode(n, 1)
+        return B, Pi, inI, kktModeViolation
+
+    def _solveAugmentedTensor(self, xsubs, B, Pi, n, maxInnerIters):
+        # now that we are done, we can calculate the new
+        # 'unaugmented matricization'
+        Chat = np.multiply(B[xsubs, :], Pi)
+        print "in solveAugmentedTensor"
+        _, _, inI2, kktMV2 = self.solveSubproblem(Chat, AUG_LOCATION, n,
+                                                  maxInnerIters)
+        self.adjust_aug_factors(n)
+        self.normalizeAugTensor()
+        return inI2, kktMV2
+
+    def _solveSignalTensor(self, xsubs, n, maxInnerIters):
+        BHat = self.cp_decomp[AUG_LOCATION].U[n]
+        Psi = tensorTools.calculatePi(self.obs_tensor,
+                                      self.cp_decomp[AUG_LOCATION], n)
+        C = np.multiply(BHat[xsubs, :], Psi)
+        print "in solveSignalTensor"
+        return self.solveSubproblem(C, REG_LOCATION, n, maxInnerIters)
+
+    def _project_factor(self, n, thr, loc=REG_LOCATION):
+        self.cp_decomp[loc].U[n] = project_A(self.cp_decomp[loc].U[n], thr)
+        return np.dot(self.cp_decomp[loc].U[n],
+                      np.diag(self.cp_decomp[loc].lmbda))
+
+    def get_signal_factors(self):
+        return self.cp_decomp[REG_LOCATION]
+
+    def compute_decomp(self, **kwargs):
+        # initialization options
+        gamma = kwargs.pop('gamma', list(np.repeat(0, self.dim_num)))
+        gradual = kwargs.pop('gradual', _DEF_GRADUAL)
+        M = kwargs.pop('init', _DEF_MINIT)
+        max_inner = kwargs.pop('max_inner', _DEF_MAXINNER)
+        max_iters = kwargs.pop('max_iter', _DEF_MAXITER)
+        delta_tol = kwargs.pop('del_tol', _DEF_DELTATOL)
+        debug_log = "Iteration {0}: Xi = {1}, dll = {2}, time = {3}"
+        print "delta_tol = \n", delta_tol
+        print "gradual = \n", gradual
+        print "max_inner = \n", max_inner
+        print "max_iters = \n", max_iters
+        print "gamma = \n", gamma
+
+        print "begin initialize\n"
+        self.initialize(M)
+        # Dictionary to manage iteration information
+        print "begin orderDict\n"
+        iterInfo = OrderedDict(sorted({}.items(), key=lambda t: t[1]))
+        print "begin calculate_ll\n"
+        lastLL = calculate_ll(self.obs_tensor, self.cp_decomp)
+        # projection factor starts at 0 (unless there's no gradual)
+        xi = 0 if gradual else 1
+        # if nothing is set, we're just not going to do any hard-thresholding
+        # for outer iterations
+        print "begin iterations\n"
+        for iteration in range(max_iters):
+            startIter = time.time()
+            print "iteration = \n", iteration
+            print "startIter = \n", startIter
+            for n in range(self.dim_num):
+                startMode = time.time()
+                # first we calculate the "augmented" tensor matricization
+                self.cp_decomp[AUG_LOCATION].redistribute(n)
+                xsubs = self.obs_tensor.subs[:, n]
+                B, Pi, inI1, kktMV1 = self._solveSignalTensor(xsubs, n,
+                                                              max_inner)
+                # hard threshold based on the xi and gamma
+                thr = xi * gamma[n]
+                if (thr > 0):
+                    B = self._project_factor(n, thr)
+                elapsed1 = time.time() - startMode
+                # calculate the new 'unaugmented matricization'
+                print "in solve decompo\n"
+                inI2, kktMV2 = self._solveAugmentedTensor(xsubs, B, Pi, n,
+                                                          max_inner)
+                elapsed2 = time.time() - startMode
+                ll = calculate_ll(self.obs_tensor, self.cp_decomp)
+                iterInfo[str((iteration, n))] = {
+                    "Time": [elapsed1, elapsed2],
+                    "KKTViolation": [kktMV1, kktMV2],
+                    "Iterations": [inI1, inI2],
+                    "LL": ll
+                }
+            if gradual:
+                xi = compute_xi(lastLL, ll, xi)
+            print "Iteration " + str(iteration) + ": " + str(ll)
+            print(debug_log.format(iteration, xi, np.abs(lastLL - ll),
+                                   time.time() - startIter))
+            print "lastLL = \n", lastLL
+            print "ll = \n", ll
+            print "abs(lastLL - ll) = \n", np.abs(lastLL - ll)
+            if np.abs(lastLL - ll) < delta_tol and xi >= 0.99:
+                break
+            lastLL = ll
+        return iterInfo, ll
+
+    def project_data(self, XHat, n, **kwargs):
+        max_iters = kwargs.pop('max_iter', _DEF_MAXITER)
+        max_inner = kwargs.pop('max_inner', _DEF_MAXINNER)
+        delta_tol = kwargs.pop('delta_tol', _DEF_DELTATOL)
+
+        print "max_iters = \n", max_iters
+        print "max_inner = \n", max_inner
+        print "delta_tol = \n", delta_tol
+        # store off the old ones
+        origM = {REG_LOCATION: self.cp_decomp[REG_LOCATION].copy(),
+                 AUG_LOCATION: self.cp_decomp[AUG_LOCATION].copy()}
+        origX = self.obs_tensor
+        self.obs_tensor = XHat
+        print "obs_tensot shape = \n", self.obs_tensor.shape
+        print "XHat shape = \n", XHat.shape
+        # randomize the nth
+        aug_fact = np.random.rand(self.obs_tensor.shape[n], 1)
+        print "cp_rank = \n", self.cp_rank
+        self.cp_decomp[REG_LOCATION].lmbda = np.ones(self.cp_rank)
+        # self.cp_decomp[AUG_LOCATION].U[n] = aug_fact
+        self.cp_decomp[REG_LOCATION].U[n] = np.random.rand(self.obs_tensor.shape[n], self.cp_rank)
+        self.cp_decomp[AUG_LOCATION].lmbda = np.ones(1)
+        # renormalize
+        self.cp_decomp[REG_LOCATION].normalize(1)
+        self.normalizeAugTensor()
+        print "self.obs_tensor shape = \n", self.obs_tensor.shape
+        print "self.cp_decomp = \n", self.cp_decomp
+        # lastLL = calculate_ll(self.obs_tensor, self.cp_decomp)
+        for iteration in range(max_iters):
+            print "project_data iteration: = \n", iteration
+            startIter_project = time.time()
+            print "startIter = \n", startIter_project
+            xsubs = self.obs_tensor.subs[:, n]
+            print "xsubs shape = \n", xsubs.shape
+            B, Pi, _, _ = self._solveSignalTensor(xsubs, n, max_inner)
+            # print "B shape = \n", B.shape
+            # print "Pi shape = \n", Pi.shape
+            # print "project dimension n = \n", n
+            self._solveAugmentedTensor(xsubs, B, Pi, n, max_inner)
+            # ll = calculate_ll(self.obs_tensor, self.cp_decomp)
+            # if np.abs(lastLL - ll) < delta_tol:
+            #    break
+            # lastLL = ll
+        # scale by summing across the rows
+        projMat = tensorTools.norm_rows(self.cp_decomp[REG_LOCATION].U[n])
+        biasMat = self.cp_decomp[AUG_LOCATION].U[n]
+        self.cp_decomp = origM
+        self.obs_tensor = origX
+        return projMat, biasMat
---
+++ examples/data-process/python/Marble-code/tensorTools_print.py
@@ -0,0 +1,272 @@
+"""
+File that contains common methods for tensor methods
+"""
+import numpy as np
+from sklearn import preprocessing
+from munkres import Munkres
+import sys
+from numba import jit
+
+import accumarray
+from sptensor import sptensor
+from dtensor import dtensor
+import khatrirao
+import tenmat
+
+
+def random_factors(modeDim, R):
+    """
+    Randomly initialize column normalized matrices
+    """
+    tmp = [np.random.rand(m, R) for m in modeDim]
+    return [preprocessing.normalize(fm, axis=0, norm='l1') for fm in tmp]
+
+
+def uniform_factors(modeDim, R):
+    """
+    Initialize the matrix using a uniform distribution
+    """
+    tmp = [np.ones((m, R)) for m in modeDim]
+    return [preprocessing.normalize(fm, axis=0, norm='l1') for fm in tmp]
+
+
+def count_nnz(U, axis):
+    """
+    Count the number of non-zero elements for each axis
+    """
+    return (np.apply_along_axis(np.count_nonzero, axis, U)).tolist()
+
+
+def count_ktensor_nnz(M, axis=0):
+    """
+    Count the number of non-zero elements for each mode in the ktensor
+    """
+    return [count_nnz(M.U[mode], axis) for mode in range(M.ndims())]
+
+
+def int_overflow(val):
+    return np.float(val)
+
+
+def get_norm(v, normtype):
+    """
+    Get the norm of a vector
+    """
+    val = np.linalg.norm(v, ord=normtype)
+    if val == 0:
+        val = 1
+    if not np.isfinite(val):
+        print val
+    # fix overflow
+    val = int_overflow(val)
+    return val
+
+
+def col_normalize(matrix, normtype=1):
+    colNorm = np.apply_along_axis(get_norm, 0, matrix, normtype)
+    return matrix / colNorm[np.newaxis, :]
+
+
+def l1_normalize(matrix):
+    """
+    Stochastic normalization
+    """
+    colNorm = matrix.sum(axis=0)
+    # deal with zero norm case
+    zeroNorm = np.where(colNorm == 0)[0]
+    colNorm[zeroNorm] = 1
+    matrix = matrix / colNorm[np.newaxis, :]
+    return matrix, colNorm
+
+
+def calc_cosine_sim(A, B):
+    """
+    Calculate the similarity between two matrices
+    """
+    return abs(np.dot(A.transpose(), B))
+
+
+def calc_tensor_sim(orig_A, new_A):
+    rawC = [calc_cosine_sim(orig_A[n], new_A[n])
+            for n in range(len(orig_A))]
+    return np.prod(np.asarray(rawC), axis=0), rawC
+
+
+def calc_weight_sim(orig_L, new_L):
+    R = len(orig_L)
+    la = np.tile(orig_L, (R, 1))
+    lb = np.tile(new_L, (R, 1)).T
+    min_entries = 1e-10 * np.ones((la.shape))
+    diff = np.abs(la - lb) / np.maximum(np.maximum(la, lb),
+                                        min_entries)
+    return np.ones((R, R)) - diff
+
+
+def opt_index(C):
+    copyC = np.ones(C.shape) - C.copy()
+    hAlg = Munkres()
+    indexes = hAlg.compute(copyC)
+    rowIdx, colIdx = map(np.array, zip(*indexes))
+    return rowIdx, colIdx, indexes
+
+
+def hardThresholdMatrix(U, thresh):
+    """
+    Perform hard thresholding of a matrix
+
+    Parameters
+    ------------
+    U : the matrix to threshold
+    thresh : the threshold value (anything below is chopped to zero)
+
+    Output
+    -----------
+    U: the new thresholded matrix
+    """
+    # do a quick sanity check
+    if not np.all(np.isfinite(U)):
+        raise ValueError("Error in U" + str(U))
+    zeroIdx = np.where(U < thresh)
+    U[zeroIdx] = 0
+    return U
+
+
+def range_omit_k(N, k):
+    """
+    Return the range of numbers from 0 -> N but omitting k
+    """
+    return [i for i in range(N) if i != k]
+
+
+def _row_norm(row):
+    """
+    Normalize such that elements sum to 1
+    """
+    weight = np.sum(row)
+    if weight < 1e-50:
+        # create a vector of ones
+        row = np.ones(row.shape)
+        weight = np.sum(row)
+    return row / weight
+
+
+def norm_rows(M):
+    """
+    Apply the normalize of a row along the axis
+    """
+    return np.apply_along_axis(_row_norm, 1, M)
+
+
+def calculatePi(X, M, n):
+    """
+    Calculate the product of all matrices but the n-th
+    (Eq 3.6 in Chi + Kolda ArXiv paper)
+    """
+    Pi = None
+    modes_but_n = range_omit_k(X.ndims(), n)
+    if isinstance(X, sptensor):
+        Pi = np.ones((X.nnz(), M.R))
+        for nn in modes_but_n:
+            Pi = np.multiply(M.U[nn][X.subs[:, nn], :], Pi)
+    else:
+        Pi = khatrirao.khatrirao_array([M.U[i] for i in modes_but_n],
+                                       reverse=True)
+    return Pi
+
+
+def calculatePhi(X, B, Pi, n, epsilon=1e-4, C=None):
+    """
+    Calculate the matrix for multiplicative update
+
+    Parameters
+    ----------
+    X       : the observed tensor
+    B       : the factor matrix associated with mode n
+    Pi      : the product of all matrices but the n-th from above
+    n       : the mode that we are trying to solve the subproblem for
+    epsilon : the
+    C       : the augmented / non-augmented tensor (\alpha u \Psi or B \Phi) in
+              sparse form
+    """
+    print "in phi X shape = \n", X.shape
+    print "in Phi Pi shape = \n", Pi.shape
+    Phi = None
+    if isinstance(X, sptensor):
+        print "is instance!!!"
+        Phi = -np.ones((X.shape[n], B.shape[1]))
+        xsubs = X.subs[:, n]
+        if C is not None:
+            v = np.sum(np.multiply(B[xsubs, :], Pi) + C, axis=1)
+        else:
+            v = np.sum(np.multiply(B[xsubs, :], Pi), axis=1)
+        wvals = X.vals.flatten() / v
+        for r in range(B.shape[1]):
+            Phi[:, r] = accumarray.accum_np(xsubs,
+                                            np.multiply(wvals, Pi[:, r]),
+                                            size=X.shape[n])
+    else:
+        Xn = tenmat.tenmat(X, [n])
+        V = np.inner(B, Pi)
+        W = Xn.data / np.maximum(V, epsilon)
+        print "in phi W shape = \n", W.shape
+        print "in phi Pi shape = \n", Pi.shape
+        Phi = np.inner(W, Pi.transpose())
+    print "tensortools in phi return Phi shape = \n", Phi.shape
+    return Phi
+
+
+def lsqr_fit(X, M):
+    """
+    Calculate the fraction of the residual explained by the factorization
+    Parameters
+    ------------
+    X : observed tensor
+    M : factorized tensor
+    """
+    normX = X.norm()
+    normresidual = np.sqrt(np.square(normX) + np.square(M.norm()) -
+                           2 * M.innerprod(X))
+    fit = 1 - (normresidual / normX)
+    return fit
+
+
+def _calc_sptensor_kl(X, M, N):
+    xsubs = X.subs
+    # absorb lambda into mode
+    C = (M.U[0] * M.lmbda[np.newaxis, :])[xsubs[:, 0], :]
+    for n in range(1, N):
+        C = np.multiply(C, M.U[n][xsubs[:, n], :])
+    mhat_vals = np.sum(C, axis=1)
+    ll = np.sum(M.U[0] * M.lmbda[np.newaxis, :]) - \
+        np.sum(np.multiply(X.vals.flatten(),
+                           np.log(mhat_vals)))
+    return ll
+
+
+@jit(nopython=True)
+def _calc_dtensor_kl(Xdata, Mdata):
+    return np.sum(Mdata - Xdata * np.log(Mdata))
+
+
+def gen_kl_fit(X, M):
+    """
+    Computes the log-likelihood of model M given data X.
+    Specifically, ll = -(sum_i m_i - x_i * log_i) where i is a
+    multiindex across all tensor dimensions
+
+    Parameters
+    ----------
+    X : input tensor of the class tensor or sptensor
+    M : list of tensor decompositiosn (note that M can be single or many)
+
+    Returns
+    -------
+    out : log likelihood value
+    """
+    if isinstance(X, sptensor):
+        return _calc_sptensor_kl(X, M, X.ndims())
+    else:
+        MHat = M
+        if not isinstance(M, dtensor):
+            MHat = M.to_dtensor()
+        return _calc_dtensor_kl(X._data.flatten(), MHat._data.flatten())
