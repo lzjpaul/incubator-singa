f12f3d28dff6f6c4eb6163d450d66b2a10f20fe8
zhaojing <zhaojing@comp.nus.edu.sg>
2016-02-05 00:29:37 +0800

    CMS CNN

---
+++ tool/python/examples/datasets/CMS2SRCKB.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from singa.model import *
+
+def load_data(
+         workspace = None, 
+         backend = 'kvfile',
+         shape = (1, 24, 338)
+      ):
+
+  # using cifar10 dataset
+  data_dir = '/ssd/zhaojing/cnn/CMS/subsample1/normrange_DIAGPROC_KB'
+  path_train = data_dir + '/train_data.bin'
+  path_test  = data_dir + '/test_data.bin'
+  path_valid = data_dir + '/valid_data.bin'
+  if workspace == None: workspace = data_dir
+
+  store = Store(path=path_train, backend=backend,
+              batchsize=100,
+              shape=shape) 
+
+  data_train = Data(load='recordinput', phase='train', conf=store)
+
+  store = Store(path=path_test, backend=backend,
+              batchsize=100,
+              shape=shape) 
+
+  data_test = Data(load='recordinput', phase='test', conf=store)
+
+  store = Store(path=path_valid, backend=backend,
+              batchsize=100,
+              shape=shape) 
+
+  data_valid = Data(load='recordinput', phase='val', conf=store)
+
+  return data_train, data_test, data_valid, workspace
