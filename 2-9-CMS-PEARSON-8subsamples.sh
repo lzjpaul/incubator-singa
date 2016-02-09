7c068e672154bf102fb67b317d140fb991e45eba
zhaojing <zhaojing@comp.nus.edu.sg>
2016-02-09 18:12:30 +0800

    best models 8 subsamples

---
+++ 2-9-CMS-PEARSON-8subsamples.sh
@@ -0,0 +1,8 @@
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-1.sh
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-2.sh
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-3.sh
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-4.sh
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-5.sh
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-6.sh
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-7.sh
+./bin/singa-run.sh -conf examples/CMS-NUH-best-models/CMS-CNN-PEARSON-8.sh
