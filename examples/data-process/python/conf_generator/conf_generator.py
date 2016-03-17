64e15cd05d76824102506c8c8c666fb2c551455b
zhaojing <zhaojing@comp.nus.edu.sg>
2016-03-17 21:21:19 +0800

    KB group

--- examples/data-process/python/conf_generator/conf_generator.py
+++ examples/data-process/python/conf_generator/conf_generator.py
@@ -10,13 +10,18 @@ import sys
 import shutil
 
 originfile = sys.argv[1]
+prefix = sys.argv[2]
 lr_array = np.array([0.1, 0.01, 0.001, 0.0001])
 decay_array = np.array([0.1, 0.01, 0.001, 0.0001])
 for i in range(len(lr_array)):
     for j in range(len(decay_array)):
-        shutil.copy(originfile,originfile+"lr"+str(i)+"decay"+str(j))
-f = open(originfile+"lr"+str(0)+"decay"+str(0), 'a+b')
-f.write("\nupdater {\n  type:kAdaGrad\n  learning_rate{\n    base_lr: " + str(lr_array[0]) + "\n    type: kFixed\n  }\n  momentum: 0.9\n  weight_decay: " + str(decay_array[0]) + "\n}")
-f.write("\ncluster {\n  nworker_groups: 1\n  nserver_groups: 1\n  nworkers_per_group: 1\n  nservers_per_group: 1\n  nworkers_per_procs: 1\n  nservers_per_procs: 1\n  workspace: \"/ssd/zhaojing/cnn/NUHALLCOND/result/" + originfile+"lr"+str(i)+"decay"+str(j) + "\"")
-print ("\ncluster {\n  nworker_groups: 1\n  nserver_groups: 1\n  nworkers_per_group: 1\n  nservers_per_group: 1\n  nworkers_per_procs: 1\n  nservers_per_procs: 1\n  workspace: \"/ssd/zhaojing/cnn/NUHALLCOND/result/" + originfile+"lr"+str(i)+"decay"+str(j) + "\"\n}" )
-f.close()
+        shutil.copy(originfile,prefix+"lr"+str(i)+"decay"+str(j)+".conf")
+        f = open(prefix+"lr"+str(i)+"decay"+str(j)+".conf", 'a+b')
+        f.write("\nupdater {\n  type:kAdaGrad\n  learning_rate{\n    base_lr: " + str(lr_array[i]) + "\n    type: kFixed\n  }\n  momentum: 0.9\n  weight_decay: " + str(decay_array[j]) + "\n}")
+        f.write("\ncluster {\n  nworker_groups: 1\n  nserver_groups: 1\n  nworkers_per_group: 1\n  nservers_per_group: 1\n  nworkers_per_procs: 1\n  nservers_per_procs: 1\n  workspace: \"/ssd/zhaojing/cnn/NUHALLCOND/result/3-17-multigroup/" + prefix+"lr"+str(i)+"decay"+str(j) + "\"\n}" )
+        print ("\ncluster {\n  nworker_groups: 1\n  nserver_groups: 1\n  nworkers_per_group: 1\n  nservers_per_group: 1\n  nworkers_per_procs: 1\n  nservers_per_procs: 1\n  workspace: \"/ssd/zhaojing/cnn/NUHALLCOND/result/3-17-multigroup/" + prefix+"lr"+str(i)+"decay"+str(j) + "\"\n}" )
+        f.close()
+        workspace = "/ssd/zhaojing/cnn/NUHALLCOND/result/3-17-multigroup/" + prefix+"lr"+str(i)+"decay"+str(j)
+        if not os.path.exists(workspace):
+            os.mkdir(workspace)
+# python conf_generator.py 3-6-KB-adaptive-diff-filters-nodrop-multigroup-400.conf multigroup400
