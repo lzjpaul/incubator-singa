d5b801a3ff560026de34defcfd9a25d18d89e687
zhaojing <zhaojing@comp.nus.edu.sg>
2016-06-07 01:15:45 +0800

    cross validation

---
+++ examples/data-process/python/Marble-code/tmp_source_file/predictionTools-lr-crossvalidation.py
@@ -0,0 +1,44 @@
+import itertools
+import numpy as np
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.svm import SVC
+from sklearn import cross_validation
+import sys
+
+
+def readData(filename):
+    return [line.rstrip().split(',') for line in open(filename)]
+
+# train_label = readData("/data/zhaojing/marble/Marble_08_09_Car_Cla_UMLS_train_label.csv")
+file = open(sys.argv[2])
+trainY = np.genfromtxt(file, delimiter=",")
+print "trainY shape = \n", trainY.shape
+
+# train_data = readData("/data/zhaojing/marble/membership_train_50_5.txt")
+file = open(sys.argv[1])
+
+trainX = np.genfromtxt(file, delimiter=",")
+print "trainX shape = \n", trainX.shape
+
+model = LogisticRegression(penalty = 'l1', tol=0.000001)
+# model.fit(trainX, trainY.T)
+# cross validation
+scores = cross_validation.cross_val_score(model, trainX, trainY, cv=5, scoring='roc_auc')
+print "roc_auc: ", scores
+scores = cross_validation.cross_val_score(model, trainX, trainY, cv=5, scoring='accuracy')
+print "accuracy: ", scores
+# scores = cross_validation.cross_val_score(model, trainX, trainY, cv=5, scoring='average_precision')
+# print "precision: ", scores
+scores = cross_validation.cross_val_score(model, trainX, trainY, cv=5, scoring='f1')
+print "f1: ", scores
+
+
+
+
+# python predictionTools.py /data/zhaojing/marble/tensor/dataset1/Marble_split_train_label_1.csv
+# /home/singa/zhaojing/NUH-singa/incubator-singa/examples/data-process/python/Marble-code/membershipresult/membership_train_S1_50_3.txt
+# /home/singa/zhaojing/NUH-singa/incubator-singa/examples/data-process/python/Marble-code/membershipresult/membership_test_S1_50_3.txt
+# /data/zhaojing/marble/tensor/dataset1/Marble_split_test_label_1.csv
+# python predictionTools.py /data/zhaojing/marble/tensor/dataset1/Marble_split_train_label_1.csv /home/singa/zhaojing/NUH-singa/incubator-singa/examples/data-process/python/Marble-code/membershipresult-10000/membershipresult/membership_train_S1_50_3.txt /home/singa/zhaojing/NUH-singa/incubator-singa/examples/data-process/python/Marble-code/membershipresult-10000/membershipresult/membership_test_S1_50_3.txt /data/zhaojing/marble/tensor/dataset1/Marble_split_test_label_1.csv
