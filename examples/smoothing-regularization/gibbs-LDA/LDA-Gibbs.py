04400485685fc37ac01545a712d2f4124ec04d7f
zhaojing <zhaojing@comp.nus.edu.sg>
2017-02-03 17:55:19 +0800

    LDA can run

--- examples/smoothing-regularization/gibbs-LDA/LDA-Gibbs.py
+++ examples/smoothing-regularization/gibbs-LDA/LDA-Gibbs.py
@@ -25,22 +25,24 @@ def log_gamma(alpha):
     if alpha < 1:
         print "ak is too small (small than 0.5)"
         exit(1)
-    if ((alpha - np.int(alpha)) == 0) # integer
+    if ((alpha - np.int(alpha)) == 0): # integer
         res = 0
         if alpha == 1:
             return res
         else: #alpha >= 2
             alpha = alpha - 1
-            while alpha >= 1
+            while alpha >= 1:
                 res += np.log(alpha)
                 alpha = (alpha - 1)
     else: #1.5, 2.5, 3.5 ...
-        alphan = (alpha - 0.5)
+        # alphan = (alpha - 0.5)
+        alphan = np.int(alpha)
         res = 0
         for n in range((alphan + 1), (2 * alphan + 1)):
             res += np.log(n)
         res -= alphan * np.log(4)
         res += 0.5 * np.log(np.pi)
+    return res
 
 
 def log_multi(alpha, K=None):
@@ -59,7 +61,7 @@ def gamma_division(ak):
         print "ak is too small (small than 0.5)"
         exit(1)
 
-    if ((ak-np.int(ak)) == 0.5) # 2.5, 3.5, ...
+    if ((ak-np.int(ak)) == 0.5): # 2.5, 3.5, ...
         ak = np.int(ak) # the least is (1.5 - 0.5) == 1 ....
         product = 1.0 / np.power(np.pi, 0.5)
         t = (2 * ak - 1)
@@ -81,7 +83,7 @@ def gamma_division(ak):
 
 class LdaSampler(object):
 
-    def __init__(self, n_gaussians, alpha=0.1, a, b):
+    def __init__(self, n_gaussians, alpha=0.1, a=1, b=2):
         """
         n_topics: desired number of topics
         alpha: a scalar (FIXME: accept vector of size n_topics)
@@ -90,13 +92,14 @@ class LdaSampler(object):
         self.n_gaussians = n_gaussians
         self.a = a
         self.b = b
+        self.alpha = alpha
 
     def _initialize(self, weight_vec):
         # number of times gaussian k is sampled
         self.nk = np.zeros(self.n_gaussians)
         # variance of the weight for each gaussian
         self.nbk = np.zeros(self.n_gaussians)
-        for k in range(nbk.shape[0])
+        for k in range(self.nbk.shape[0]):
             self.nbk[k] = self.b
         self.gaussians = {} # which gaussian this weight dimension refers to
 
@@ -106,10 +109,10 @@ class LdaSampler(object):
             self.nbk[pi] += (weight_vec[w] * weight_vec[w] * 0.5)
             self.gaussians[w] = pi
 
-    def weight_cond_pi(w, k, weight_vec):
-        term1 = np.power((1 - (0.5 * weight_vec[w] * weight_vec[w] / (self.nbk[k] + 0.5 * weight_vec[w] * weight_vec[w]))), (self.a + nk[k] / 2.0))
+    def _weight_cond_pi(self, w, k, weight_vec):
+        term1 = np.power((1 - (0.5 * weight_vec[w] * weight_vec[w] / (self.nbk[k] + 0.5 * weight_vec[w] * weight_vec[w]))), (self.a + self.nk[k] / 2.0))
         term2 = 1.0 / np.power((self.nbk[k] + 0.5 * weight_vec[w] * weight_vec[w]), 0.5)
-        term3 = gamma_division((self.a + nk[k] / 2.0))
+        term3 = gamma_division((self.a + self.nk[k] / 2.0))
         return (term1 * term2 * term3)
 
     def _conditional_distribution(self, w, weight_vec):
@@ -118,8 +121,8 @@ class LdaSampler(object):
         """
         left = np.zeros(self.nk.shape[0])
         for k in range(self.nk.shape[0]):
-            left[k] = weight_cond_pi(w, k, weight_vec)
-        right = (self.nk + self.a)
+            left[k] = self._weight_cond_pi(w, k, weight_vec)
+        right = (self.nk + self.alpha)
         p_pi = left * right
         # normalize to obtain probabilities
         p_pi /= np.sum(p_pi)
@@ -132,16 +135,16 @@ class LdaSampler(object):
         lik = 0
 
         for k in xrange(self.n_gaussians):
-            lik += (nk[k] / 2.0) * (-1) * np.log(2 * np.pi)
+            lik += (self.nk[k] / 2.0) * (-1) * np.log(2 * np.pi)
             lik += log_gamma(self.a + (self.nk[k] / 2.0))
-            lik -= (self.a + (self.nk[k] / 2.0)) * np.log(nbk[k])
+            lik -= (self.a + (self.nk[k] / 2.0)) * np.log(self.nbk[k])
 
-        lik += log_multi(self.nk+self.a)
-        lik -= log_multi(self.a, self.n_gaussians)
+        lik += log_multi(self.nk+self.alpha)
+        lik -= log_multi(self.alpha, self.n_gaussians)
 
         return lik
 
-    def run(self, weight_vec, maxiter=30, eps=0.0001):
+    def run(self, weight_vec, maxiter=5, eps=0.0001):
         """
         Run the Gibbs sampler.
         """
@@ -149,6 +152,7 @@ class LdaSampler(object):
 
         joint_liklihood = 0
         for it in xrange(maxiter):
+            print "iteration: ", it
             for w in range(weight_vec.shape[0]):
                 self.nk[self.gaussians[w]] -= 1
                 self.nbk[self.gaussians[w]] -= (weight_vec[w] * weight_vec[w] * 0.5)
@@ -161,26 +165,29 @@ class LdaSampler(object):
                 self.gaussians[w] = pi # !!!
 
             if it == 0:
-                joint_liklihood = loglikelihood()
+                joint_liklihood = self.loglikelihood()
             else:
-                cur_joint_liklihood = loglikelihood()
+                cur_joint_liklihood = self.loglikelihood()
                 if np.absolute(cur_joint_liklihood -joint_liklihood) < eps:
                     break
                 joint_liklihood = cur_joint_liklihood
 
 
-        theta_vec = (self.nk + self.a) / (weight_vec.shape[0] + 1)
+        theta_vec = (self.nk + self.alpha) / (weight_vec.shape[0] + 1)
         lambda_vec = np.zeros(self.nk.shape[0])
         for k in range (lambda_vec.shape[0]):
-            lambda_vec[k] = (self.a + 0.5 * nk[k]) / (self.b + 0.5 * nbk[k])
+            lambda_vec[k] = (self.a + 0.5 * self.nk[k]) / (self.b + 0.5 * self.nbk[k])
 
         return theta_vec, lambda_vec
 
 if __name__ == "__main__":
 
+    n_gaussians = int(sys.argv[1])
+    weight_vec = np.random.rand(1000)
     print "weight_vec.shape[0]: ", weight_vec.shape[0] #one-dimension array
     sampler = LdaSampler(n_gaussians, alpha = 1.0/(weight_vec.shape[0]), a = 2, b = 5) #number of gaussians
     theta_vec, lambda_vec = sampler.run(weight_vec)
     print "theta_vec: ", theta_vec
+    print "sum(theta_vec): ", sum(theta_vec)
     print "lambda_vec: ", lambda_vec
     # print "Likelihood", sampler.loglikelihood()
