e010511a2e5c88c88cad67d6512cda581a2af6fe
zhaojing <zhaojing@comp.nus.edu.sg>
2017-08-11 11:05:47 +0800

    modify chi square expected and studnet t equl-var

--- examples/CNN-readmission/p-value/cal_p_value.py
+++ examples/CNN-readmission/p-value/cal_p_value.py
@@ -12,21 +12,24 @@ from scipy.stats import chisquare
 
 def cal_statistics(data):
     print "processing: ", i
-    print "data: ", data
+    # print "data: ", data
     data_readmitted = data[data[:,-1]==1]
     data_not_readmitted = data[data[:,-1]==0]
-    print "data_readmitted: ", data_readmitted
-    print "data_not_readmitted: ", data_not_readmitted
+    # print "data_readmitted: ", data_readmitted
+    # print "data_not_readmitted: ", data_not_readmitted
     data_readmitted_ratio = np.sum(data_readmitted[:,0]) / len(data_readmitted[:,0])
     data_not_readmitted_ratio = np.sum(data_not_readmitted[:,0]) / len(data_not_readmitted[:,0])
     print "data_readmitted_ratio: ", data_readmitted_ratio
     print "data_not_readmitted_ratio: ", data_not_readmitted_ratio
     data_observed = data[data[:,0]==1]
-    print "data_observed: ", data_observed
-    print "observed number: ", len(data_observed[:,-1])
+    # print "data_observed: ", data_observed
+    # print "observed number: ", len(data_observed[:,-1])
     print "observed readmitted: ", np.sum(data_observed[:,-1])
     print "observed not readmitted: ", (len(data_observed[:,-1]) - np.sum(data_observed[:,-1]))
-    p_value = chisquare([np.sum(data_observed[:,-1]), len(data_observed[:,-1]) - np.sum(data_observed[:,-1])])
+    print "expected readmitted: ", (394/1755.0)*len(data_observed[:,-1])
+    print "expected not readmitted: ", (1361/1755.0)*len(data_observed[:,-1])
+    p_value = chisquare([np.sum(data_observed[:,-1]), len(data_observed[:,-1]) - np.sum(data_observed[:,-1])], \
+                        f_exp=[(394/1755.0)*len(data_observed[:,-1]), (1361/1755.0)*len(data_observed[:,-1])])
     print "p value: ", p_value
 
 
@@ -42,7 +45,7 @@ if __name__ == '__main__':
     file = open(args.data)
     data = np.genfromtxt(file, dtype = 'str', delimiter=",")
     file.close()
-    print "data = \n",data
+    # print "data = \n",data
     print "data shape = \n", data.shape
     print "data.dtype = \n", data.dtype
 
@@ -51,7 +54,7 @@ if __name__ == '__main__':
 
     if args.disorcont == 0: #discrete
     	categorical_data = np.zeros((len(data[:,0]), len(data[0,:])))
-    	print "categorical_data.shape: \n", categorical_data.shape
+    	# print "categorical_data.shape: \n", categorical_data.shape
 
     	categorical_data[:, -1] = data[:, -1] #label column
 
@@ -63,11 +66,11 @@ if __name__ == '__main__':
     	    print "dummies: ", b
     	    categorical_data[:, i] = b.values.argmax(1)
 
-    	print "categorical_data: \n", categorical_data
-    	print "categorical_data shape: \n", categorical_data.shape
+    	# print "categorical_data: \n", categorical_data
+    	# print "categorical_data shape: \n", categorical_data.shape
 
     	onehot_data = OneHotEncoder(categorical_features=[0],sparse=False).fit_transform(categorical_data.astype(int))
-    	print "onehot_data", onehot_data
+    	# print "onehot_data", onehot_data
 
     	for i in range(len(onehot_data[0,:])-1):
     	    cal_statistics(onehot_data[:, [i, -1]])
@@ -76,8 +79,8 @@ if __name__ == '__main__':
     else: #continuous variable
         data_readmitted = data[data[:,-1]=='1']
         data_not_readmitted = data[data[:,-1]=='0']
-        print "data_readmitted: ", data_readmitted[:10]
-        print "data_not_readmitted: ", data_not_readmitted[:10]
+        # print "data_readmitted: ", data_readmitted[:10]
+        # print "data_not_readmitted: ", data_not_readmitted[:10]
         data_readmitted = data_readmitted.astype(np.int)
         data_not_readmitted = data_not_readmitted.astype(np.int)
         readmitted_mean = np.mean(data_readmitted[:, 0])
@@ -89,7 +92,9 @@ if __name__ == '__main__':
         print "not_readmitted_mean: ", not_readmitted_mean
         print "not_readmitted_std: ", not_readmitted_std
         data = data.astype(np.int)
-        p_value = stats.ttest_ind(data[:, 0], data[:, 1])[1]
+        print "data[:, 0]: ", data[:, 0]
+        print "data[:, 1]: ", data[:, 1]
+        p_value = stats.ttest_ind(data[:, 0], data[:, 1], equal_var = False)[1]
         print "p_value: ", p_value
 
 
