e0fc375501dc8e40ffbe3a771eadbfa939433445
zhaojing <zhaojing@comp.nus.edu.sg>
2017-08-25 00:02:06 +0800

    SCRIPTS: fine-tune alexnet

---
+++ examples/GM-prior-cifar10/archive_code/8-23-resnet-finetune/GMM-DL-four-2.sh
@@ -0,0 +1,1 @@
+python gm_prior_train.py -maxepoch 10 -gmnum 4 -gmuptfreq 100 -paramuptfreq 50 -gpuid 0 -hyperparampath script_generator/8-23/hyperparams/lambda_0_a_0_alpha_1_b_0.csv -resultpath /data/zhaojing/regularization/log0823/GMM-DL-four/GMM-DL-four_lambda_0_a_0_alpha_1_b_0_result alexnet cifar-10-batches-py/ | tee -a /data/zhaojing/regularization/log0823/GMM-DL-four/GMM-DL-four-2.log
