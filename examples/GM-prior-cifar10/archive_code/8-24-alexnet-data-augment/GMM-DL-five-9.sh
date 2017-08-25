8f9d53d2a5f4ff0cf2f2620289bfb3400d0af229
zhaojing <zhaojing@comp.nus.edu.sg>
2017-08-25 21:34:02 +0800

    SCRIPTS: alexnet with data augmentation

---
+++ examples/GM-prior-cifar10/archive_code/8-24-alexnet-data-augment/GMM-DL-five-9.sh
@@ -0,0 +1,1 @@
+python gm_prior_train.py -maxepoch 10 -gmnum 4 -gmuptfreq 100 -paramuptfreq 50 -gpuid 0 -hyperparampath script_generator/8-24/hyperparams/lambda_0_a_1_alpha_1_b_1.csv -resultpath /data/zhaojing/regularization/log0824/GMM-DL-five/GMM-DL-five_lambda_0_a_1_alpha_1_b_1_result.txt alexnet cifar-10-batches-py/ | tee -a /data/zhaojing/regularization/log0824/GMM-DL-five/GMM-DL-five-9.log
---
+++ examples/GM-prior-cifar10/archive_code/8-24-alexnet-data-augment/GMM-DL-five-baseline.sh
@@ -0,0 +1,1 @@
+python train.py alexnet cifar-10-batches-py/ | tee -a /data/zhaojing/regularization/log0824/GMM-DL-five/GMM-DL-five-baseline.log
