f553ace6a6e88fca0a042e4e3e2b86e981713401
zhaojing <zhaojing@comp.nus.edu.sg>
2017-12-21 13:46:09 +0800

    first version lda mlp, not debug yet

---
+++ examples/LDA-MLP/lda_mlp_model.py
@@ -0,0 +1,40 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# =============================================================================
+from singa import layer
+from singa import metric
+from singa import loss
+from singa import initializer
+# from singa import net as myffnet
+import mynet as myffnet
+import Multi_SigmoidCrossEntropyLoss
+import AUC_Accuracy_Metric
+
+def create_net(use_cpu=False):
+    if use_cpu:
+        layer.engine = 'singacpp'
+
+    net = myffnet.ProbFeedForwardNet(Multi_SigmoidCrossEntropyLoss.MultiSigmoidCrossEntropy(), AUC_Accuracy_Metric.AUCAccuracy())
+    net.add(layer.Dense('dense', 128))
+    net.add(layer.Dense('dense', 80))
+    for (pname, pvalue) in zip(net.param_names(), net.param_values()):
+        if len(pvalue.shape) > 1:
+            initializer.gaussian(pvalue, pvalue.shape[0], pvalue.shape[1])
+        else:
+            pvalue.set_value(0)
+        print pname, pvalue.l1()
+    return net
